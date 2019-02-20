b4199758e90dce03e5a8b8e353a2c89526c0005c
Aaron Martin <aaron.martin@shopgate.com>
2019-02-20 15:04:13 -0600

    CCP-1059: adjusted props passed to BaseProductCharacteristics

--- src/components/ProductCharacteristics/index.jsx
+++ src/components/ProductCharacteristics/index.jsx
@@ -2,4 +2,7 @@ import React from 'react';
 import BaseProductCharacteristics from '@shopgate/pwa-common/components/ProductCharacteristics';
 import withProductContext from '../../connectors/withProductContext';
 
-export default withProductContext(props => <BaseProductCharacteristics {...props} />);
+export default withProductContext(({ productContext, render }) => {
+  const newProps = { ...productContext, render };
+  return <BaseProductCharacteristics {...newProps} />;
+});
